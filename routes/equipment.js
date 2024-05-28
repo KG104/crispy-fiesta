ddf7f45143ebe89ea6636dd238e12d35268e4909
KG104 <konstantin.gerlach@kabelmail.de>
2024-05-28 13:52:57 +0200

    adding routes, models and controllers

--- routes/equipment.js
+++ routes/equipment.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var usersController = require('../controllers/usersController');
+var equipmentController =  require('../controllers/equipmentController');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
 
 });
