f908551cab02e0d532984380871c673b682725f5
deluxe8484 <leo.knittel@informatik.hs-fulda.de>
2024-06-09 21:36:21 +0200

    Update borrowsController.js
    erstmal nur den controller geändert

--- controllers/borrowsController.js
+++ controllers/borrowsController.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
+const crypto = require('crypto');
+
 // read data from json file
-let blogs = JSON.parse(fs.readFileSync('./models/borrows.json'));
+let borrows = JSON.parse(fs.readFileSync('./models/borrows.json'));
 
 function generateID() {
     return crypto.randomUUID();
@@ -8,38 +10,67 @@ function generateID() {
 
 // get all of the borrows
 const getAllBorrows = (req, res) => {
-
+    res.json(borrows);
 }
 
 // add a new borrow
 const addBorrow = (req, res) => {
-    id = generateID();
+    const { userID, borrowDate, returnDate, equipmentID } = req.body;
+    
+    if (!userID || !borrowDate || !returnDate || !equipmentID) {
+        return res.status(404).json({ message: "Incomplete data" });
+    }
+    
+     const id = generateID();
+     let addBorrow = {
+         borrowID: id,
+         userID,
+         borrowDate,
+         returnDate,
+         equipmentID
+     };
+    
+    borrows.push(addBorrow);
+     fs.writeFileSync('./models/borrows.json', JSON.stringify(borrows, null, 2));
+     res.status(200).json(addBorrow);
 }
 
 // get one specific borrow by its id
 const getBorrow = (req, res) => {
-
+    const borrowID = req.params.id;
+    const searchedBorrow = borrows.find(elementb => elementb.borrowID === borrowID);
+    if (!searchedBorrow) {
+        return res.status(404).json({ message: "Borrow not found" });
+    }
+    res.status(200).json(searchedBorrow);
 }
 
 // update one specific borrow by its id
 const updateBorrow = (req, res) => {
+    borrows = JSON.parse(fs.readFileSync('./models/borrows.json'));
+    var borrowToUpdate = borrows.find(elementb => elementb.borrowID == req.params.id);
+    console.log(borrows);
+    // if a user with this id exists
+    if (borrowToUpdate !== undefined) {
+        // update the keys
+        borrowToUpdate.userID = req.body.userID;
+        borrowToUpdate.equipmentID = req.body.equipmentID;
+        fs.writeFileSync('./models/borrows.json', JSON.stringify(borrows, null, 2));
+        res.status(200).json(borrows);
+    } else res.status(404).send("Der gesuchte Ausleihvorgang existiert nicht ):");
 
 }
 
 // delete one specific borrow by its id
 const deleteBorrow = (req, res) => {
+    // filter out the borrow with the specified id
+    borrows = borrows.filter(elementb => elementb.borrowID !== req.params.id);
+    // overwrite the json file 
+    fs.writeFileSync('./models/borrows.json', JSON.stringify(borrows, null, 2));
+    res.status(200).json(borrows);
 
 }
 
-/*const typeMap = {
-    "Borrows": o([
-        { json: "id", js: "id", typ: "" },
-        { json: "borrowerID", js: "borrowerID", typ: "" },
-        { json: "borrowDate", js: "borrowDate", typ: Date },
-        { json: "returnDate", js: "returnDate", typ: Date },
-        { json: "equipmentID", js: "equipmentID", typ: "" },
-    ], false),
-};*/
 
 module.exports = {
     getAllBorrows,
